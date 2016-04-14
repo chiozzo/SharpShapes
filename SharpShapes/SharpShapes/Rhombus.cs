d5fe0c4e8006d2abbc2a4a63c7b7a18586f89e34
Martin Cross <mncross@gmail.com>
2016-04-14 09:50:15 -0500

    add class for rhombus

---
+++ SharpShapes/SharpShapes/Rhombus.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpShapes
+{
+    class Rhombus : Shape
+    {
+        public double horizontalDiagonal { get; set; }
+        public double verticalDiagonal { get; set; }
+
+        public override double calculateArea()
+        {
+            return ( horizontalDiagonal * verticalDiagonal ) / 2;
+        }
+
+        // Fun Fact: a 3D Rhombus is called a Rhombohedron.
+        public override double calculateVolume()
+        {
+            // V = a³ * (1-cos(α)) * √(1+2cos(α))
+            return base.calculateVolume();
+        }
+    }
+}
