9e66d1123c83f280f927ea1646e2111101f504cc
Martin Cross <mncross@gmail.com>
2016-04-14 09:48:17 -0500

    add class for circle, sphere, and cylinder

---
+++ SharpShapes/SharpShapes/Circle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpShapes
+{
+    class Circle : Shape
+    {
+        public double radius { get; set; }
+        double pi = 3.14;
+
+        public override double calculateArea()
+        {
+            return pi * ( radius * radius );
+        }
+
+        public double calculateSphereVolume()
+        {
+            return (4/3) * pi * ( radius * radius * radius );
+        }
+
+        public double calculateCylinderVolume()
+        {
+            return this.calculateArea() * height;
+        }
+    }
+}
